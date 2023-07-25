777dee23861632874bb23a49e52a67f22d630ddf
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-26 01:57:26 +0300

    implement User State View

--- Zstagram/Core/Components/UserStateView.swift
+++ Zstagram/Core/Components/UserStateView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct UserStateView: View {
+    
+    let value: Int
+    let title: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("\(value)")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            Text(title)
+                .font(.footnote)
+        }
+        .frame(width: 72)
     }
 }
 
 struct UserStateView_Previews: PreviewProvider {
     static var previews: some View {
-        UserStateView()
+        UserStateView(value: 20, title: "Posts")
     }
 }
