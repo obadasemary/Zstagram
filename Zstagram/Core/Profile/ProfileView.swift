34094e82c59f7cb52ea2ddc621c03a1d59f9c555
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-26 01:12:23 +0300

    Add edit button

--- Zstagram/Core/Profile/ProfileView.swift
+++ Zstagram/Core/Profile/ProfileView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // header
+            VStack {
+                // image and stats
+                HStack {
+                    
+                }
+                
+                // name and bio
+                VStack {
+                    Text("iOS Developer")
+                    
+                    Text("Fintech")
+                }
+                
+                // action button
+                Button {
+                    
+                } label: {
+                    Text("Edit Profile")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(
+                            width: 360,
+                            height: 32
+                        )
+                        .foregroundColor(.black)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(.gray, lineWidth: 1)
+                        )
+                }
+                
+                Divider()
+            }
+            
+            // post grid view
+        }
     }
 }
 
