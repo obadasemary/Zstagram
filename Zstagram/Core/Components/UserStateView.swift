1bc78813b6a8822b24d99647490efd5f79abf2d6
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-26 01:56:23 +0300

    Add UserStateView

---
+++ Zstagram/Core/Components/UserStateView.swift
@@ -0,0 +1,20 @@
+//
+//  UserStateView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 26.07.2023.
+//
+
+import SwiftUI
+
+struct UserStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserStateView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserStateView()
+    }
+}
