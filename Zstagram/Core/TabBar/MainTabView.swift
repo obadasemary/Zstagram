1311c2de1f1f87ccedab2e669ef88eb3b28b4b0e
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-26 00:52:40 +0300

    add tab bar

---
+++ Zstagram/Core/TabBar/MainTabView.swift
@@ -0,0 +1,42 @@
+//
+//  MainTabView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 26.07.2023.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            Text("UploadPost")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            Text("Notification")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
