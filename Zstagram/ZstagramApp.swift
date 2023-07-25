177c7825a4c8e0ab9ce8368e25fa431e35558148
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-25 16:19:26 +0300

    Initial Commit

---
+++ Zstagram/ZstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZstagramApp.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
