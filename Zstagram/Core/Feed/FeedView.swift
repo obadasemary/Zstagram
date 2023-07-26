86cbde49f7ad9c93fc363ffe219c866a4ac5bdfb
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-26 17:50:02 +0300

    Add some assets

---
+++ Zstagram/Core/Feed/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 26.07.2023.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
