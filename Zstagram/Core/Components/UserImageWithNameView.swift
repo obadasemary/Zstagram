0f304ada822a4dee86dea60a51d532cef3615cd1
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-28 20:20:28 +0300

    introduce UserImageWithNameView

--- Zstagram/Core/Components/UserImageWithNameView.swift
+++ Zstagram/Core/Components/UserImageWithNameView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct UserImageWithNameView: View {
+    
+    let image: String
+    let name: String
+    let title: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .fontWeight(.semibold)
+                
+                Text(title)
+            }
+            .font(.footnote)
+            
+            Spacer()
+        }
+        .padding(.horizontal)
     }
 }
 
 struct UserImageWithNameView_Previews: PreviewProvider {
     static var previews: some View {
-        UserImageWithNameView()
+        UserImageWithNameView(
+            image: "Obada",
+            name: "Obada",
+            title: "Senior iOS Developer"
+        )
     }
 }
