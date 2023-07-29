2d1847e0ada7b1032994d5250a6092bb358b7c18
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-30 01:38:29 +0300

    implement login view

--- Zstagram/Core/Authentication/LoginView.swift
+++ Zstagram/Core/Authentication/LoginView.swift
@@ -15,33 +15,108 @@ struct LoginView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-//
-//                Spacer()
-//
-                Text("Zstagram")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .fontDesign(.monospaced)
-                
                 VStack {
-                    TextField("Enter your email", text: $email)
-                        .autocorrectionDisabled()
-                        .font(.subheadline)
-                        .padding(12)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 24)
+                    Spacer(minLength: UIScreen.main.bounds.height / 5)
                     
-                    SecureField("Password", text: $password)
-                        .autocorrectionDisabled()
-                        .font(.subheadline)
-                        .padding(12)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .padding(.horizontal, 24)
+                    Text("Zstagram")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .fontDesign(.monospaced)
+                    
+                    VStack {
+                        TextField("Enter your email", text: $email)
+                            .autocorrectionDisabled()
+                            .font(.subheadline)
+                            .padding(12)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(10)
+                            .padding(.horizontal, 24)
+                        
+                        SecureField("Password", text: $password)
+                            .autocorrectionDisabled()
+                            .font(.subheadline)
+                            .padding(12)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(10)
+                            .padding(.horizontal, 24)
+                    }
+                    
+                    Button {
+                        print("Show forgot password")
+                    } label: {
+                        Text("Forgot Password?")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .padding(.top)
+                            .padding(.trailing, 28)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    
+                    Button {
+                        print("Login")
+                    } label: {
+                        Text("Login")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width: 360, height: 44)
+                            .background(Color(.systemBlue))
+                            .cornerRadius(8)
+                    }
+                    .padding(.vertical)
+                    
+                    HStack {
+                        Rectangle()
+                            .frame(
+                                width: (UIScreen.main.bounds.width / 2) - 40 ,
+                                height: 0.5
+                            )
+                        
+                        Text("OR")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Rectangle()
+                            .frame(
+                                width: (UIScreen.main.bounds.width / 2) - 40 ,
+                                height: 0.5
+                            )
+                    }
+                    .foregroundColor(.gray)
+                    
+                    Button {
+                        print("Continue with Instagram")
+                    } label: {
+                        HStack {
+                            Image("instagram-logo")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                            
+                            Text("Continue with Instagram")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                        }
+                        .padding(.top, 8)
+                    }
+                    
+                    Spacer(minLength: UIScreen.main.bounds.height / 4)
+                    
+                    Divider()
+                    
+                    NavigationLink {
+                        Text("Sign Up")
+                    } label: {
+                        HStack(spacing: 3) {
+                            
+                            Text("Don't have an account?")
+                            Text("Sign Up")
+                                .fontWeight(.semibold)
+                        }
+                        .font(.footnote)
+                    }
+                    .padding(.vertical, 16)
                 }
-            
-
             }
         }
     }
