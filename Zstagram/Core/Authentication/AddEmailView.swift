d8990aa5497eef4e1330067f2f6b8d5cea2b00ea
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-30 01:55:20 +0300

    implement Email view

--- Zstagram/Core/Authentication/AddEmailView.swift
+++ Zstagram/Core/Authentication/AddEmailView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct AddEmailView: View {
+    
+    @State private var email: String = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 12) {
+                Text("Add your email")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(.top)
+                
+                Text("you'll use this email to sign in to your account")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 24)
+                
+                TextField("Enter your email", text: $email)
+                    .autocorrectionDisabled()
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                    .padding(.top)
+                
+                Button {
+                    print("Next")
+                } label: {
+                    Text("Next")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(8)
+                }
+                .padding(.vertical)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
     }
 }
 
