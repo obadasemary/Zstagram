0b898569d418af4c8bfb1ffdc61c659e23ab705b
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-30 01:42:34 +0300

    add email view

---
+++ Zstagram/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,20 @@
+//
+//  AddEmailView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 30.07.2023.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddEmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddEmailView()
+    }
+}
