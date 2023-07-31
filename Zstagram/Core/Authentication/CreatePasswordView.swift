8b7d267423d88bc4f45e82edcca8e125da8b2141
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-01 00:04:31 +0300

    Introduced create password view

---
+++ Zstagram/Core/Authentication/CreatePasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePasswordView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 31.07.2023.
+//
+
+import SwiftUI
+
+struct CreatePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatePasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatePasswordView()
+    }
+}
