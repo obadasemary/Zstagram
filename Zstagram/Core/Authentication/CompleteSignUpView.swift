182a7fe341c13daca523260dc3280dfa3d143322
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-01 00:21:09 +0300

    introduced CompleteSignUpView

---
+++ Zstagram/Core/Authentication/CompleteSignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  CompleteSignUpView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 1.08.2023.
+//
+
+import SwiftUI
+
+struct CompleteSignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompleteSignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompleteSignUpView()
+    }
+}
