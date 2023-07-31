8e91de5ead469d9654b02a7146f917afdcafb55a
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-31 23:50:53 +0300

    Introduced Create username view

---
+++ Zstagram/Core/Authentication/CreateUsernameView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateUsernameView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 31.07.2023.
+//
+
+import SwiftUI
+
+struct CreateUsernameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateUsernameView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateUsernameView()
+    }
+}
