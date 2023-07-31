5375f01748875bc57d91ecbeb7ab1ea416751a64
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-01 00:05:14 +0300

    implement CreatePasswordView

--- Zstagram/Core/Authentication/CreatePasswordView.swift
+++ Zstagram/Core/Authentication/CreatePasswordView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct CreatePasswordView: View {
+    
+    @State private var password: String = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 12) {
+                Text("Create a password")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(.top)
+                
+                Text("Your password must be at least 6 characters in length.")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 24)
+                
+                SecureField("Password", text: $password)
+                    .autocorrectionDisabled()
+                    .modifier(IGTextFieldModifier())
+                    .padding(.top)
+                
+                NavigationLink {
+//                    CreateUsernameView()
+                } label: {
+                    Text("Next")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(8)
+                }
+                .padding(.vertical)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
     }
 }
 
