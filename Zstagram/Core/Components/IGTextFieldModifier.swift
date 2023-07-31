d19e1a5c78ea0d8fe7f935ba1456f7c9bb266255
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-31 23:36:09 +0300

    implement IGTextFieldModifier

--- Zstagram/Core/Components/IGTextFieldModifier.swift
+++ Zstagram/Core/Components/IGTextFieldModifier.swift
@@ -5,4 +5,16 @@
 //  Created by Abdelrahman Mohamed on 31.07.2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct IGTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal, 24)
+    }
+}
