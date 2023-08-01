34e5391ae04f859ee345093366890d2688c5783c
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-01 06:45:01 +0300

    make profile ready to use
    With the user model, when the user taps on any profile from the search view, it will directly open the profile of the chosen user profile.

--- Zstagram/Core/Profile/ProfileView.swift
+++ Zstagram/Core/Profile/ProfileView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ProfileView: View {
     
+    @Environment(\.dismiss) var dismiss
+    let user: User
+    
     private let gridItems: [GridItem] = [
         .init(.flexible(), spacing: 1),
         .init(.flexible(), spacing: 1),
@@ -16,77 +19,80 @@ struct ProfileView: View {
     ]
     
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                VStack {
-                    HStack {
-                        Image("Obada")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 80, height: 80)
-                            .clipShape(Circle())
-                        
-                        Spacer()
-                        
-                        HStack(spacing: 8) {
-                            UserStateView(value: 3, title: "Posts")
-                            UserStateView(value: 12, title: "Followers")
-                            UserStateView(value: 24, title: "Following")
-                        }
-                        
-                        Spacer()
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom, 4)
+        ScrollView {
+            VStack {
+                HStack {
+                    Image(user.profileImageUrl ?? "awsAI")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 80, height: 80)
+                        .clipShape(Circle())
                     
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("Abdelrahman Mohamed")
+                    Spacer()
+                    
+                    HStack(spacing: 8) {
+                        UserStateView(value: 3, title: "Posts")
+                        UserStateView(value: 12, title: "Followers")
+                        UserStateView(value: 24, title: "Following")
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.bottom, 4)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    if let fullname = user.fullname {
+                        Text(fullname)
                             .fontWeight(.semibold)
-                        Text("Senior iOS Developer | FinTech | E-Commerce Mobile Solutions")
-                    }
-                    .font(.footnote)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-                    
-                    Button {
-                        
-                    } label: {
-                        Text("Edit Profile")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .frame(
-                                width: 360,
-                                height: 32
-                            )
-                            .foregroundColor(.black)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 6)
-                                    .stroke(.gray, lineWidth: 1)
-                            )
                     }
                     
-                    Divider()
+                    if let bio = user.bio {
+                        Text(bio)
+                    }
+                }
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+                
+                Button {
+                    
+                } label: {
+                    Text("Edit Profile")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(
+                            width: 360,
+                            height: 32
+                        )
+                        .foregroundColor(.black)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(.gray, lineWidth: 1)
+                        )
                 }
                 
-                LazyVGrid(columns: gridItems, spacing: 1) {
-                    ForEach(0 ... 109, id: \.self) { item in
-                        Image("Obada")
-                            .resizable()
-                            .scaledToFill()
-                    }
+                Divider()
+            }
+            
+            LazyVGrid(columns: gridItems, spacing: 1) {
+                ForEach(0 ... 109, id: \.self) { item in
+                    Image("Obada")
+                        .resizable()
+                        .scaledToFill()
                 }
             }
-            .navigationTitle("Profile")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "line.3.horizontal")
-                            .foregroundColor(.black)
-                    }
-
+        }
+        .navigationTitle(user.username)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
                 }
             }
         }
@@ -95,6 +101,6 @@ struct ProfileView: View {
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView()
+        ProfileView(user: User.MOCK_USERS[0])
     }
 }
