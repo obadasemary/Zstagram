f06f6d23e0bc49dab7f33be440e22d064de3bc87
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-01 06:38:30 +0300

    introduced CurrentUserProfileView

---
+++ Zstagram/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,100 @@
+//
+//  ProfileView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 26.07.2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Image("Obada")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStateView(value: 3, title: "Posts")
+                            UserStateView(value: 12, title: "Followers")
+                            UserStateView(value: 24, title: "Following")
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    .padding(.bottom, 4)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Abdelrahman Mohamed")
+                            .fontWeight(.semibold)
+                        Text("Senior iOS Developer | FinTech | E-Commerce Mobile Solutions")
+                    }
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(
+                                width: 360,
+                                height: 32
+                            )
+                            .foregroundColor(.black)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(.gray, lineWidth: 1)
+                            )
+                    }
+                    
+                    Divider()
+                }
+                
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0 ... 109, id: \.self) { item in
+                        Image("Obada")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                }
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundColor(.black)
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
