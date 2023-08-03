e0f8ab9e664a9ddc6c09251a1ac957407cd2e3a1
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-03 20:30:30 +0300

    introduced Profile Header View

---
+++ Zstagram/Core/Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 3.08.2023.
+//
+
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHeaderView()
+    }
+}
