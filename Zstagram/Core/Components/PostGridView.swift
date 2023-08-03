eed6dc76272c0f8253f8084825efe2b133d740ef
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-03 20:36:10 +0300

    implement PostGridView

--- Zstagram/Core/Components/PostGridView.swift
+++ Zstagram/Core/Components/PostGridView.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 struct PostGridView: View {
     
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
+    private let imageDimension: CGFloat = (UIScreen.main.bounds.width / 3) - 1
+    
+    var posts: [Post]
+    
+    var body: some View {
+        LazyVGrid(columns: gridItems, spacing: 1) {
+            ForEach(posts) { post in
+                Image(post.imageUrl)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: imageDimension, height: imageDimension)
+                    .clipped()
+            }
+        }
+    }
 }
 
 struct PostGridView_Previews: PreviewProvider {
     static var previews: some View {
+        ScrollView {
+            PostGridView(posts: Post.MOCK_POSTS)
+        }
     }
 }
