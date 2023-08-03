0902c1fd8b67b989283154302ac4fd5844d47f58
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-03 20:50:40 +0300

    Introduced UploadPostView

---
+++ Zstagram/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,20 @@
+//
+//  UploadPostView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 3.08.2023.
+//
+
+import SwiftUI
+
+struct UploadPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UploadPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        UploadPostView()
+    }
+}
