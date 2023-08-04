2b15e8278781bd0db2cc3016944424bd95657461
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-04 07:05:41 +0300

    implement CGFloat+Extensions with imageDimension

--- Zstagram/Extensions/CGFloat+Extensions.swift
+++ Zstagram/Extensions/CGFloat+Extensions.swift
@@ -5,4 +5,10 @@
 //  Created by Abdelrahman Mohamed on 4.08.2023.
 //
 
-import Foundation
+import SwiftUI
+
+extension CGFloat {
+    static func imageDimension() -> CGFloat {
+        (UIScreen.main.bounds.width / 3) - 1
+    }
+}
