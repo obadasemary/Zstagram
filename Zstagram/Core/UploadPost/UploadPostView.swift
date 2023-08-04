fa66e166a97bacd309f25417772414db83df36e0
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-04 06:43:38 +0300

    implement adding new photo and cancel with returning back to the feed view

--- Zstagram/Core/UploadPost/UploadPostView.swift
+++ Zstagram/Core/UploadPost/UploadPostView.swift
@@ -5,16 +5,90 @@
 //  Created by Abdelrahman Mohamed on 3.08.2023.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct UploadPostView: View {
+    
+    @State private var isLoading: Bool = true
+    private let imageDimension: CGFloat = (UIScreen.main.bounds.width / 3) - 1
+    @State private var caption = ""
+    @State private var imagePickerPresented = false
+    @StateObject var viewModel = UploadPostViewModel()
+    @Binding var tabIndex: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                HStack(spacing: 8) {
+                    if let image = viewModel.postImage {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: imageDimension, height: imageDimension)
+                            .clipped()
+                            .cornerRadius(8)
+                    } else {
+                        Image("instagram-logo")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: imageDimension, height: imageDimension)
+                            .clipped()
+                            .redacted(reason: isLoading ? .placeholder : [])
+                            .cornerRadius(8)
+                    }
+                    
+                    TextField(
+                        "Enter your caption...",
+                        text: $caption,
+                        axis: .vertical
+                    )
+                }
+                .padding()
+            }
+            .navigationTitle("New Post")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        caption = ""
+                        viewModel.selectedImage = nil
+                        viewModel.postImage = nil
+                        tabIndex = 0
+                    } label: {
+                        Image(systemName: "xmark.app")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.black)
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "paperplane")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            imagePickerPresented.toggle()
+            if viewModel.postImage != nil {
+                self.isLoading = false
+            }
+        }
+        .photosPicker(
+            isPresented: $imagePickerPresented,
+            selection: $viewModel.selectedImage
+        )
+        .tint(.black)
     }
 }
 
 struct UploadPostView_Previews: PreviewProvider {
     static var previews: some View {
-        UploadPostView()
+        UploadPostView(tabIndex: .constant(0))
     }
 }
