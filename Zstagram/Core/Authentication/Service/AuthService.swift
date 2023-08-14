cf44edb49aace4190d32af28785c90fe8cd3f323
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-14 23:25:25 +0300

    Implement loadUserData
    Via current user data, if it exists

--- Zstagram/Core/Authentication/Service/AuthService.swift
+++ Zstagram/Core/Authentication/Service/AuthService.swift
@@ -13,11 +13,14 @@ import FirebaseFirestoreSwift
 class AuthService {
     
     @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: User?
     
     static let shared = AuthService()
     
     init() {
-        self.userSession = Auth.auth().currentUser
+        Task {
+            try await loadUserData()
+        }
     }
     
     @MainActor
@@ -28,6 +31,7 @@ class AuthService {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
             self.userSession = result.user
+            try await loadUserData()
         } catch {
             print("DEBUG: Failed to log in user with error \(error.localizedDescription)")
         }
@@ -50,17 +54,24 @@ class AuthService {
         }
     }
     
+    @MainActor
     func loadUserData() async throws {
-        
+        self.userSession = Auth.auth().currentUser
+        guard let currentUId = userSession?.uid else { return }
+        let snapshot = try await Firestore.firestore().collection("users").document(currentUId).getDocument()
+        print("DEBUG: Snapshot data is \(snapshot.data() ?? [:])")
+        self.currentUser = try? snapshot.data(as: User.self)
     }
     
     func signout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+        self.currentUser = nil
     }
     
     private func uploadUserData(uid: String, username: String, email: String) async {
         let user = User(id: uid, username: username, eamil: email)
+        self.currentUser = user
         guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
         
         try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
