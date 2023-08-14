8b9cf7e928e5489e530c590737dc95d0b2cff21e
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-14 19:05:37 +0300

    implement login in auth service

--- Zstagram/Core/Authentication/Service/AuthService.swift
+++ Zstagram/Core/Authentication/Service/AuthService.swift
@@ -22,8 +22,12 @@ class AuthService {
         withEmail email: String,
         password: String
     ) async throws {
-        
-        
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+        } catch {
+            print("DEBUG: Failed to log in user with error \(error.localizedDescription)")
+        }
     }
     
     func createUser(
