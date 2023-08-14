a094252e82860a981b5aec05e90189d30b397269
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-14 20:47:10 +0300

    introduced LoadingView

---
+++ Zstagram/Core/Components/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  Zstagram
+//
+//  Created by Abdelrahman Mohamed on 14.08.2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
