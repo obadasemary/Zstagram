463b2779408da235b1de0cb957a50e2aa3b18d8e
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-14 20:40:57 +0300

    add isLoading published property to registration view model

--- Zstagram/Core/Authentication/ViewModel/RegistrationViewModel.swift
+++ Zstagram/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -8,17 +8,21 @@
 import Foundation
 import Combine
 
+@MainActor
 class RegistrationViewModel: ObservableObject {
     
     @Published var username = ""
     @Published var email = ""
     @Published var password = ""
+    @Published var isLoading = false
     
     func createUser() async throws {
+        isLoading = true
         try await AuthService.shared.createUser(
             email: email,
             password: password,
             username: username
         )
+        isLoading = false
     }
 }
