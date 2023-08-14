d7c334ceefabbc0de1aff028d2646c0771b8c1df
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-14 16:33:35 +0300

    implement content view model

--- Zstagram/Core/Root/ViewModel/ContentViewModel.swift
+++ Zstagram/Core/Root/ViewModel/ContentViewModel.swift
@@ -4,3 +4,26 @@
 //
 //  Created by Abdelrahman Mohamed on 14.08.2023.
 //
+
+import Foundation
+import Firebase
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        setupSubscribers()
+    }
+    
+    func setupSubscribers() {
+        service.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+}
