fd6c0f441113756efb42643536f126f375c9e911
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-14 17:44:10 +0300

    implement RegistrationViewModel

--- Zstagram/Core/Authentication/ViewModel/RegistrationViewModel.swift
+++ Zstagram/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import Combine
+
+class RegistrationViewModel: ObservableObject {
+    
+    @Published var username = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    func createUser() async throws {
+        try await AuthService.shared.createUser(
+            email: email,
+            password: password,
+            username: username
+        )
+    }
+}
