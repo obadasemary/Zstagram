441bd13f0d6406f7dc289858b12a172c47ce04d9
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-14 18:57:17 +0300

    implement login view model

--- Zstagram/Core/Authentication/ViewModel/LoginViewModel.swift
+++ Zstagram/Core/Authentication/ViewModel/LoginViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import Combine
+
+class LoginViewModel: ObservableObject {
+    
+    @Published var email = ""
+    @Published var password = ""
+    
+    func signIn() async throws {
+        try await AuthService.shared.login(
+            withEmail: email,
+            password: password
+        )
+    }
+}
