0e5586eb147bdd06b99ae2318889354fac0b3480
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-15 01:33:14 +0300

    using real users instead

--- Zstagram/Core/Search/View/SearchView.swift
+++ Zstagram/Core/Search/View/SearchView.swift
@@ -10,23 +10,30 @@ import SwiftUI
 struct SearchView: View {
     
     @State private var searchText = ""
+    @StateObject var viewModel = SearchViewModel()
     
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVStack(spacing: 12) {
-                    ForEach(User.MOCK_USERS) { user in
-                        NavigationLink(value: user) {
-                            UserImageWithNameView(
-                                image: user.profileImageUrl ?? "awsAI",
-                                name: user.username,
-                                fullName: user.fullname ?? nil
-                            )
+                ZStack {
+                    LazyVStack(spacing: 12) {
+                        ForEach(viewModel.users) { user in
+                            NavigationLink(value: user) {
+                                UserImageWithNameView(
+                                    image: user.profileImageUrl ?? "awsAI",
+                                    name: user.username,
+                                    fullName: user.fullname ?? nil
+                                )
+                            }
                         }
                     }
+                    .padding(.top, 8)
+                    .searchable(text: $searchText, prompt: "Search...")
+                    
+                    if viewModel.isLoading {
+                        LoadingView()
+                    }
                 }
-                .padding(.top, 8)
-                .searchable(text: $searchText, prompt: "Search...")
             }
             .navigationDestination(for: User.self, destination: { user in
                 ProfileView(
