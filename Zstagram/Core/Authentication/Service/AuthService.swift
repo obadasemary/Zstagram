5ffe169099f142a7e2005d2db6c9c93c5160a1fb
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-14 16:33:18 +0300

    implement auth service

--- Zstagram/Core/Authentication/Service/AuthService.swift
+++ Zstagram/Core/Authentication/Service/AuthService.swift
@@ -5,3 +5,41 @@
 //  Created by Abdelrahman Mohamed on 14.08.2023.
 //
 
+import Foundation
+import FirebaseAuth
+
+class AuthService {
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+    }
+    
+    func login(
+        withEmail email: String,
+        password: String
+    ) async throws {
+        
+        
+    }
+    
+    func createUser(
+        email: String,
+        password: String,
+        username: String
+    ) async throws {
+        
+        
+    }
+    
+    func loadUserData() async throws {
+        
+    }
+    
+    func signout() {
+        
+    }
+}
