cf071b3300fa548678242fefd6e45e3eedca93af
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-15 22:08:55 +0300

    Show the loading view if it is still loading.

--- Zstagram/Core/Profile/View/EditProfileView.swift
+++ Zstagram/Core/Profile/View/EditProfileView.swift
@@ -20,55 +20,52 @@ struct EditProfileView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack {
-                    PhotosPicker(selection: $viewModel.selectedImage) {
-                        VStack {
-                            if let image = viewModel.profileImage {
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(
-                                        width: CGFloat.imageDimension(),
-                                        height: CGFloat.imageDimension()
-                                    )
-                                    .foregroundColor(.white)
-                                    .background(.gray)
-                                    .clipShape(Circle())
-                            } else {
-                                Image(systemName: "person")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(
-                                        width: CGFloat.imageDimension(),
-                                        height: CGFloat.imageDimension()
-                                    )
-                                    .foregroundColor(.white)
-                                    .background(.gray)
-                                    .clipShape(Circle())
+                ZStack {
+                    VStack {
+                        PhotosPicker(selection: $viewModel.selectedImage) {
+                            VStack {
+                                if let image = viewModel.profileImage {
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(
+                                            width: CGFloat.imageDimension(),
+                                            height: CGFloat.imageDimension()
+                                        )
+                                        .foregroundColor(.white)
+                                        .background(.gray)
+                                        .clipShape(Circle())
+                                } else {
+                                    CircularProfileImageView(user: viewModel.user, size: .xLarge)
+                                }
+                                
+                                Text("Edit profile picture")
+                                    .font(.footnote)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.black)
+                                
+                                Divider()
                             }
+                        }
+                        .padding(.vertical, 8)
+                        
+                        VStack {
+                            EditProfileRowView(
+                                title: "Name",
+                                placeholder: "Enter your name...",
+                                text: $viewModel.fullname
+                            )
                             
-                            Text("Edit profile picture")
-                                .font(.footnote)
-                                .fontWeight(.semibold)
-                                .foregroundColor(.black)
-                            
-                            Divider()
+                            EditProfileRowView(
+                                title: "Bio",
+                                placeholder: "Enter you bio...",
+                                text: $viewModel.bio
+                            )
                         }
                     }
-                    .padding(.vertical, 8)
                     
-                    VStack {
-                        EditProfileRowView(
-                            title: "Name",
-                            placeholder: "Enter your name...",
-                            text: $viewModel.fullname
-                        )
-                        
-                        EditProfileRowView(
-                            title: "Bio",
-                            placeholder: "Enter you bio...",
-                            text: $viewModel.bio
-                        )
+                    if viewModel.isLoading {
+                        LoadingView()
                     }
                 }
             }
@@ -88,6 +85,7 @@ struct EditProfileView: View {
                     Button {
                         Task {
                             try await viewModel.updateUserData()
+                            dismiss()
                         }
                     } label: {
                         Image(systemName: "paperplane")
@@ -101,6 +99,6 @@ struct EditProfileView: View {
 
 struct EditProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        EditProfileView(user: User.MOCK_USERS[0])
+        EditProfileView(user: User.MOCK_USERS[1])
     }
 }
