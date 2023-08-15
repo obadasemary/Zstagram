7d74e49e2a6911eb94df945be2068bec35057adb
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-15 23:07:48 +0300

    implement upload post with caption

--- Zstagram/Core/UploadPost/ViewModel/UploadPostViewModel.swift
+++ Zstagram/Core/UploadPost/ViewModel/UploadPostViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import PhotosUI
 import SwiftUI
+import Firebase
 
 @MainActor
 class UploadPostViewModel: ObservableObject {
@@ -20,12 +21,55 @@ class UploadPostViewModel: ObservableObject {
         }
     }
     @Published var postImage: Image?
+    @Published var isLoading = false
+    
+    private var uiImage: UIImage?
     
     func loadImage(fromItem item: PhotosPickerItem?) async {
         guard let item = item else { return }
         
         guard let data = try? await item.loadTransferable(type: Data.self) else { return }
         guard let uiImage = UIImage(data: data) else { return }
+        self.uiImage = uiImage
         self.postImage = Image(uiImage: uiImage)
+    }
+    
+    func uploadPost(caption: String) async throws {
+        
+        isLoading = true
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            isLoading = false
+            return
+        }
+        
+        guard let uiImage = uiImage else {
+            isLoading = false
+            return
+        }
+        
+        let postRef = Firestore.firestore().collection("posts").document()
+        guard let imageUrl = try await ImageUploader.uploadImage(
+            image: uiImage,
+            uId: uid,
+            isProfileImage: false
+        ) else {
+            isLoading = false
+            return
+        }
+        
+        let post = Post(
+            id: postRef.documentID,
+            ownerUid: uid,
+            caption: caption,
+            likes: 0,
+            imageUrl: imageUrl,
+            timestamp: Timestamp()
+        )
+        
+        guard let encodedPost = try? Firestore.Encoder().encode(post) else { return }
+        try await postRef.setData(encodedPost)
+        
+        isLoading = false
     }
 }
