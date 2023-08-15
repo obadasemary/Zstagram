47248edd02398e520ecb728893dd96d2b03b759e
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-16 02:20:24 +0300

    implement fetch user posts with uid

--- Zstagram/Services/PostService.swift
+++ Zstagram/Services/PostService.swift
@@ -10,8 +10,24 @@ import Firebase
 
 struct PostService {
     
-    static func fetchPosts() async throws -> [Post] {
-        let snapshot = try await Firestore.firestore().collection("posts").getDocuments()
+    private static let postsCollection = Firestore.firestore().collection("posts")
+    
+    static func fetchFeedPosts() async throws -> [Post] {
+        let snapshot = try await postsCollection.getDocuments()
+        var posts = snapshot.documents.compactMap({ try? $0.data(as: Post.self) })
+        
+        for i in 0 ..< posts.count {
+            let post = posts[i]
+            let ownerUid = post.ownerUid
+            let postUser = try await UserService.fetchUser(withCurrentUId: ownerUid)
+            posts[i].user = postUser
+        }
+        
+        return  posts
+    }
+    
+    static func fetchUserPosts(uid: String) async throws -> [Post] {
+        let snapshot = try await postsCollection.whereField("ownerUid", isEqualTo: uid).getDocuments()
         return snapshot.documents.compactMap({ try? $0.data(as: Post.self) })
     }
 }
