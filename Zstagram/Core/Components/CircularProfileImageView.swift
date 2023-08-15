220f7d1c750447ce05141c485add3897476fa3c7
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-15 21:36:01 +0300

    implement CircularProfileImageView

--- Zstagram/Core/Components/CircularProfileImageView.swift
+++ Zstagram/Core/Components/CircularProfileImageView.swift
@@ -6,15 +6,56 @@
 //
 
 import SwiftUI
+import Kingfisher
+
+enum ProfileImageSize {
+    case xSmall
+    case small
+    case medium
+    case large
+    case xLarge
+    
+    var dimension: CGFloat {
+        switch self {
+        case .xSmall:
+            return 40
+        case .small:
+            return 48
+        case .medium:
+            return 64
+        case .large:
+            return 80
+        case .xLarge:
+            return 100
+        }
+    }
+}
 
 struct CircularProfileImageView: View {
+    
+    let user: User
+    let size: ProfileImageSize
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let imageUrl = user.profileImageUrl {
+            KFImage(URL(string: imageUrl))
+                .resizable()
+                .scaledToFill()
+                .frame(width: size.dimension, height: size.dimension)
+                .clipShape(Circle())
+        } else {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: size.dimension, height: size.dimension)
+                .clipShape(Circle())
+                .foregroundColor(.black)
+                .background(.white)
+        }
     }
 }
 
 struct CircularProfileImageView_Previews: PreviewProvider {
     static var previews: some View {
-        CircularProfileImageView()
+        CircularProfileImageView(user: User.MOCK_USERS[3], size: .large)
     }
 }
