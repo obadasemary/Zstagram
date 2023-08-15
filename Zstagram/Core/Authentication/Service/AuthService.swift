e5475dcef10da31630f9d5889807cf00541eb6ed
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-16 02:21:50 +0300

    clean debug log from auth service

--- Zstagram/Core/Authentication/Service/AuthService.swift
+++ Zstagram/Core/Authentication/Service/AuthService.swift
@@ -46,9 +46,7 @@ class AuthService {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
-            print("DEBUG: Did create user...")
             await uploadUserData(uid: result.user.uid, username: username, email: email)
-            print("DEBUG: Did upload user data...")
         } catch {
             print("DEBUG: Failed to register user with error \(error.localizedDescription)")
         }
