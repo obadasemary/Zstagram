b6274fc847f7cb87938dceadf0f3c124f543c93a
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-16 02:23:31 +0300

    implement post grid view model

--- Zstagram/Core/Components/ViewModel/PostGridViewModel.swift
+++ Zstagram/Core/Components/ViewModel/PostGridViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class PostGridViewModel: ObservableObject {
+    
+    private let user: User
+    @Published var posts = [Post]()
+    @Published var isLoading = false
+    
+    init(user: User) {
+        self.user = user
+        
+        Task {
+            try await fetchUserPosts()
+        }
+    }
+    
+    @MainActor
+    func fetchUserPosts() async throws {
+        isLoading = true
+        
+        self.posts = try await PostService.fetchUserPosts(uid: user.id)
+        
+        for i in 0 ..< posts.count {
+            posts[i].user = self.user
+        }
+        
+        isLoading = false
+    }
+}
