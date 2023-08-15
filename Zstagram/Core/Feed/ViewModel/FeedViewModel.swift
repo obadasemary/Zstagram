1474cd6e00cb14bc4ca4d7de6fbdbc0a6b28b3f2
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-16 00:38:39 +0300

    Implement feed view model
    I recently implemented a feed view model for my app, and it has been a game-changer. The feed view model allows me to easily display content from various sources in a seamless and organized manner. With this feature, users can quickly scroll through their feeds and stay up-to-date on the latest news, social media posts, and other updates. The view model also helps me manage the data and ensure that the app is running smoothly. Overall, I'm thrilled with the results of implementing the feed view model and can't wait to see how it continues to improve the user experience.

--- Zstagram/Core/Feed/ViewModel/FeedViewModel.swift
+++ Zstagram/Core/Feed/ViewModel/FeedViewModel.swift
@@ -4,3 +4,33 @@
 //
 //  Created by Abdelrahman Mohamed on 15.08.2023.
 //
+
+import Foundation
+import Firebase
+
+class FeedViewModel: ObservableObject {
+    
+    @Published var posts = [Post]()
+    @Published var isLoading = false
+    
+    init() {
+        Task {
+            try await fetchPosts()
+        }
+    }
+    
+    @MainActor
+    func fetchPosts() async throws {
+        isLoading = true
+        self.posts = try await PostService.fetchPosts()
+        
+        for i in 0 ..< posts.count {
+            let post = posts[i]
+            let ownerUid = post.ownerUid
+            let postUser = try await UserService.fetchUser(withCurrentUId: ownerUid)
+            posts[i].user = postUser
+        }
+        
+        isLoading = false
+    }
+}
