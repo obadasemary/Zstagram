3a32fd503b812dfd198f25e8854d74527cc2f190
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-16 00:34:34 +0300

    implement fetch User withCurrentUId

--- Zstagram/Services/UserService.swift
+++ Zstagram/Services/UserService.swift
@@ -10,6 +10,12 @@ import Firebase
 
 struct UserService {
     
+    static func fetchUser(withCurrentUId currentUId: String) async throws -> User {
+        let snapshot = try await Firestore.firestore().collection("users").document(currentUId).getDocument()
+        print("DEBUG: Snapshot data is \(snapshot.data() ?? [:])")
+        return try snapshot.data(as: User.self)
+    }
+    
     static func fetchAllUsers() async throws -> [User] {
         let snapshot = try await Firestore.firestore().collection("users").getDocuments()
         return snapshot.documents.compactMap({ try? $0.data(as: User.self) })
