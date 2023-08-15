b661aa4b8ed60955c127b8f09d0ff89aa5330ab2
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-15 20:41:37 +0300

    implement image uploader

--- Zstagram/Services/ImageUploader.swift
+++ Zstagram/Services/ImageUploader.swift
@@ -5,4 +5,24 @@
 //  Created by Abdelrahman Mohamed on 15.08.2023.
 //
 
-import Foundation
+import UIKit
+import Firebase
+import FirebaseStorage
+
+struct ImageUploader {
+    
+    static func uploadImage(image: UIImage, uId: String) async throws -> String? {
+        guard let imageData = image.pngData() else { return nil }
+        let filename = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images/\(uId)/\(filename)")
+        
+        do {
+            let _ = try await ref.putDataAsync(imageData)
+            let url = try await ref.downloadURL()
+            return url.absoluteString
+        } catch {
+            print("DEBUG: Failed to upload image with error \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
