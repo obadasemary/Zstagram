23667f455a679c0693f6c0892a81bec4922abf57
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-08-15 18:32:20 +0300

    implement EditProfileViewModel

--- Zstagram/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ Zstagram/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -5,4 +5,30 @@
 //  Created by Abdelrahman Mohamed on 15.08.2023.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
+import Firebase
+
+@MainActor
+class EditProfileViewModel: ObservableObject {
+    
+    @Published var selectedImage: PhotosPickerItem? {
+        didSet {
+            Task {
+                await loadImage(fromItem: selectedImage)
+            }
+        }
+    }
+    @Published var profileImage: Image?
+    
+    @Published var fullname = ""
+    @Published var bio = ""
+    
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item = item else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
